001d72fa6fa6b332b694be1221a0dd333d073030
JGAgain <tramsyck@gmail.com>
2019-06-16 08:58:43 +0800

    能够生成md

--- openssl/test_bio_md.c
+++ openssl/test_bio_md.c
@@ -4,5 +4,23 @@
 
 
 int main(){
-    BIO * bio;
+
+    char *msg = "hello world\n";
+    char result[EVP_MAX_MD_SIZE]="\0";
+    BIO * bio_md, *bio_null;
+
+    bio_null = BIO_new(BIO_s_null());
+    bio_md = BIO_new(BIO_f_md());
+    BIO_set_md(bio_md, EVP_md5());
+
+    BIO_push(bio_md, bio_null);
+
+    BIO_write(bio_md, msg, strlen(msg));
+    BIO_flush(bio_md);
+
+    BIO_gets(bio_md, result, EVP_MAX_MD_SIZE);
+
+    printf("%d\n", strlen(result));
+
+    return 0;
 }
