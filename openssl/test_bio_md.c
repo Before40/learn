6dd097d07070e0e9822552a6801bcd011e08963d
JGAgain <tramsyck@gmail.com>
2019-06-16 09:23:30 +0800

    base64 encode

--- openssl/test_bio_md.c
+++ openssl/test_bio_md.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
 #include <openssl/bio.h>
 #include <openssl/evp.h>
+#include <openssl/buffer.h>
 
 
 int main(){
 
     char *msg = "hello world\n";
-    char result[EVP_MAX_MD_SIZE]="\0";
+    char result[512]="\0";
     BIO * bio_md, *bio_null;
 
-    bio_null = BIO_new(BIO_s_null());
-    bio_md = BIO_new(BIO_f_md());
-    BIO_set_md(bio_md, EVP_md5());
+    BIO *bio_b64;
+    BIO *bio_mem;
 
-    BIO_push(bio_md, bio_null);
+    bio_null = BIO_new(BIO_s_null());
+
+    bio_md = BIO_new(BIO_f_md());
+    BIO_set_md(bio_md, EVP_sha1());
+
+    bio_b64 = BIO_new(BIO_f_base64());
+
+    bio_mem = BIO_new(BIO_s_mem());
+
+    bio_b64 = BIO_push(bio_b64, bio_mem);
+    bio_md = BIO_push(bio_md, bio_b64);
 
     BIO_write(bio_md, msg, strlen(msg));
     BIO_flush(bio_md);
 
-    BIO_gets(bio_md, result, EVP_MAX_MD_SIZE);
+    BUF_MEM *bptr = NULL;
+    BIO_get_mem_ptr(bio_mem, &bptr);
+    size_t len = bptr->length;
 
-    printf("%d\n", strlen(result));
+    memcpy(result, bptr->data, len);
+    printf("%s\n", result);
+    BIO_free_all(bio_md);
+    return 0;
+
+    
 
     return 0;
 }
